ddc05f7e8f6a8ae74369f4a418ebb9da7490386f
bizimunda <yourbelover@gmail.com>
2016-08-31 14:18:35 +0200

    Modified Weather URL using location manager

--- app/src/main/java/evoliris/com/evoliris_json/MainActivity.java
+++ app/src/main/java/evoliris/com/evoliris_json/MainActivity.java
@@ -1,6 +1,10 @@
 package evoliris.com.evoliris_json;
 
 import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -23,13 +27,17 @@ import evoliris.com.evoliris_json.task.GetAsyncTask;
 import evoliris.com.evoliris_json.task.GetImageAsyncTask;
 
 
-public class MainActivity extends ActionBarActivity implements GetAsyncTask.GetAsynTaskCallback{
+public class MainActivity extends ActionBarActivity implements GetAsyncTask.GetAsynTaskCallback, LocationListener{
 
     private Button btnMainRequest;
     private ProgressBar pbMainLoading;
-    private TextView tvMainCity, tvMainCountry, tvMainTemp, tvMainTime;
+    private TextView tvMainCity, tvMainCountry, tvMainTemp, tvMainTime, tvMainLong, tvMainLat;
     private ImageView imageView;
     private EditText etMainCityName;
+    private LocationManager locationManager;
+    private Location location;
+    private String provider;
+
 
 
 
@@ -44,8 +52,26 @@ public class MainActivity extends ActionBarActivity implements GetAsyncTask.GetA
         tvMainCountry = (TextView) findViewById(R.id.tv_main_country);
         tvMainTemp = (TextView) findViewById(R.id.tv_main_temp);
         tvMainTime = (TextView) findViewById(R.id.tv_main_time);
+        tvMainLat = (TextView) findViewById(R.id.tv_main_lat);
+        tvMainLong = (TextView) findViewById(R.id.tv_main_long);
         etMainCityName= (EditText) findViewById(R.id.et_main_cityName);
         imageView= (ImageView) findViewById(R.id.iv_main_icon);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the locatioin provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        // Initialize the location fields
+        if (location != null) {
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+        } else {
+            tvMainLong.setText("Location not available");
+            tvMainLat.setText("Location not available");
+        }
 
 
         btnMainRequest.setOnClickListener(new View.OnClickListener() {
@@ -61,15 +87,52 @@ public class MainActivity extends ActionBarActivity implements GetAsyncTask.GetA
 
                 if(isConnected) {
                     GetAsyncTask task = new GetAsyncTask(MainActivity.this);
-                    task.execute("http://api.openweathermap.org/data/2.5/weather?q="+etMainCityName.getText().toString()+"&units=metric&appid=9124335619b9206a59b03318f5625a3d");
+
+                    //User adds city in the EditText
+                    //task.execute("http://api.openweathermap.org/data/2.5/weather?q="+etMainCityName.getText().toString()+"&units=metric&appid=9124335619b9206a59b03318f5625a3d");
+
+                    //It locates weather using locationManager
+                    task.execute("http://api.openweathermap.org/data/2.5/weather?lat=" + tvMainLat.getText() + "&lon=" + tvMainLong.getText() + "&appid=9124335619b9206a59b03318f5625a3d&units=metric");
                 } else {
                     Toast.makeText(MainActivity.this,"No data connection", Toast.LENGTH_SHORT).show();
                 }
+                etMainCityName.setText("");
 
             }
         });
 
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        tvMainLong.setText(String.valueOf(location.getLongitude()));
+        tvMainLat.setText(String.valueOf(location.getLatitude()));
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
     }
 
 
